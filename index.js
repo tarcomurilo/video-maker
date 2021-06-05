596784214f908b4dd69b8120290bb382c31030aa
Tarço Murilo <tarcomurilo@gmail.com>
2021-06-05 11:34:54 -0300

    feat: colocar search term e prefix

---
+++ index.js
@@ -0,0 +1,26 @@
+const readline = require('readline-sync')
+
+function start() {
+    const content = {}
+  
+    content.searchTerm = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
+
+    function askAndReturnSearchTerm() {
+
+        return readline.question('Digite um termo da Wikipedia: ')
+        
+    }
+
+    function askAndReturnPrefix() {
+        const prefixes = ['Who is', 'What is', 'The history of']
+        const selectedprefixIndex = readline.keyInSelect(prefixes)
+        const selectedprefixText = prefixes[selectedprefixIndex]
+        return selectedprefixText
+    }
+
+    
+    console.log(content)
+}
+
+start()
